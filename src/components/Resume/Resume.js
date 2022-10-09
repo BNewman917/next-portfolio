864a8e708d77163b9c3fc24572a49f66b693f8b9
BNewman917 <brentnewman917@gmail.com>
2022-10-08 23:06:02 -0500

    exit button

--- src/components/Resume/Resume.js
+++ src/components/Resume/Resume.js
@@ -1,14 +1,7 @@
 import React from "react";
-import {
-    ResumeWrapper,
-    ResumeModal,
-    Download,
-    Img,
-    Exit,
-} from "./ResumeStyles";
+import { ResumeWrapper, ResumeModal, Download, Img } from "./ResumeStyles";
 import { useStateContext } from "../../context/StateContext";
 import { HiOutlineDownload } from "react-icons/hi";
-import { AiOutlineClose } from "react-icons/ai";
 
 const Resume = () => {
     const { show, setShow } = useStateContext();
@@ -18,19 +11,12 @@ const Resume = () => {
         setShow(false);
     };
 
-    const exitClick = (e) => {
-        setShow(false);
-    };
-
     return (
         <ResumeWrapper onClick={wrapperClick} className={show ? "show" : null}>
             <ResumeModal>
                 <Download href="/assets/Brent_Newman_Resume.pdf" download>
                     <HiOutlineDownload size={"30px"} />
                 </Download>
-                <Exit onMouseDown={exitClick}>
-                    <AiOutlineClose size={"30px"} />
-                </Exit>
                 <Img src="/images/resume.png" alt="resume" />
             </ResumeModal>
         </ResumeWrapper>
